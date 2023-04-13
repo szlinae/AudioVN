372ddf06c35340ac1f76533bf81a3adc6ea1cb62
Selina <szlinae@gmail.com>
2023-04-12 20:23:07 -0400

    added bg change script, imported new animation

---
+++ Assets/Scripts/BackgroundChange.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundChange : MonoBehaviour
+{
+    public SpriteRenderer[] Images;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
